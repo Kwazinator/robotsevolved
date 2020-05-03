80ec54a9da78f7454938796d9a80f32263c7ef4e
wmstens <wmstens@gmail.com>
2020-05-02 22:13:18 -0500

    remove emails
    opsec bruv

--- flaskr/insert.sql
+++ flaskr/insert.sql
@@ -2,5 +2,5 @@ DELETE from user WHERE 1=1;
 
 UPDATE SQLITE_SEQUENCE SET seq=0 WHERE name='user';
 
-INSERT into user (username,password,logintype,accountId,profilePicture,email) VALUES ('kkwasniewski','yolo','facebook','1','2','kwasiky@gmail.com');
-INSERT into user (username,password,logintype,accountId,profilePicture,email) VALUES ('someoneelse','yoasdlo','faceboasdok','3','2','kwasiky@gmail.com');
+INSERT into user (username,password,logintype,accountId,profilePicture,email) VALUES ('kkwasniewski','yolo','facebook','1','2','kwasiky@gfmail.com');
+INSERT into user (username,password,logintype,accountId,profilePicture,email) VALUES ('someoneelse','yoasdlo','faceboasdok','3','2','kwasiky@gfmail.com');
