a1dd4351881ea94c08377e77023d51f389815c12
Kwazinator <kwasiky@gmail.com>
2020-05-04 18:35:38 -0400

    Revert "Revert "new index.html template""
    This reverts commit e482ab91a39024943b2d0ca4c1da577bba751f1b.

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -5,11 +5,7 @@ import Game from './containers/Game';
 import * as serviceWorker from './serviceWorker';
 
 
-ReactDOM.render(<Game boardSize={640} playerSize={40}/>, document.getElementById('postscontainer'));
-//ReactDOM.render(<ChartZ type={'bar'}/>, document.getElementById('row2'));
-//ReactDOM.render(<ChartZ type={'line'}/>, document.getElementById('row3'));
-
-//ReactDOM.render(<Graph width={400} height={240}/>, document.getElementById('row3'));
+ReactDOM.render(<Game />, document.getElementById('postscontainer'));
 
 
 // If you want your app to work offline and load faster, you can change
