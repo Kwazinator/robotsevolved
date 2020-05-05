c4abb7574fcc91987896d0fdc11487991d323cba
Kwazinator <kwasiky@gmail.com>
2020-05-05 14:25:48 -0400

    commited

---
+++ flaskr/static/react/precache-manifest.4ba7efcd866e8ed1e093e9418ea6fb85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0fbd593bcc9a8c22429efec3cfaf3ab",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "98545be64742f72e5221",
+    "url": "/static/react/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "82bf5eec95ff0181f9e2",
+    "url": "/static/react/js/2.3a2f1b0d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/react/js/2.3a2f1b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98545be64742f72e5221",
+    "url": "/static/react/js/main.bf27a50f.chunk.js"
+  },
+  {
+    "revision": "c7241993fd2c7bed7213",
+    "url": "/static/react/js/runtime-main.b9f37f5d.js"
+  }
+]);
