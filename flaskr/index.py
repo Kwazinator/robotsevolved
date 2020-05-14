f4f3deca12942f33cd1d7d9cd89d1042e5488024
Kwazinator <kwasiky@gmail.com>
2020-05-13 23:32:38 -0400

    Update index.py

--- flaskr/index.py
+++ flaskr/index.py
@@ -21,7 +21,7 @@ def get_games_data(numMoves,Offset):
 @bp.route('/')
 @jwt_optional
 def index():
-    get_games_data_value = get_games_data(30,0)
+    get_games_data_value = get_games_data(30,20)
     userID = get_jwt_identity()
     user = None
     loggedin = 'No'
