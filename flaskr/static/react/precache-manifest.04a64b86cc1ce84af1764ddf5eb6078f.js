8e69a03816c249b4755404f768ffdc91f6190823
Kwazinator <kwasiky@gmail.com>
2020-06-05 12:20:56 -0400

    commited for docker

---
+++ flaskr/static/react/precache-manifest.04a64b86cc1ce84af1764ddf5eb6078f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989dc550d9e98b0c34db15b4cd5ebada",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "fca58337cda59d7d3500",
+    "url": "/static/react/css/main.4d983085.chunk.css"
+  },
+  {
+    "revision": "15293bc670fd86776e5c",
+    "url": "/static/react/js/2.9d0f775b.chunk.js"
+  },
+  {
+    "revision": "d7b319d8a6f4d5773bf6cc977c7731d1",
+    "url": "/static/react/js/2.9d0f775b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fca58337cda59d7d3500",
+    "url": "/static/react/js/main.ba0ec0ce.chunk.js"
+  },
+  {
+    "revision": "c7241993fd2c7bed7213",
+    "url": "/static/react/js/runtime-main.b9f37f5d.js"
+  }
+]);
