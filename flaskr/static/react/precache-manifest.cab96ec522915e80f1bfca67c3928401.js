17b156a67b753829871bf6008864380b4d5a4de9
Kyle Kwasniewski <kkwasniewski@STROZLLC.PUBLIC>
2020-07-25 15:02:17 -0400

    asdf

---
+++ flaskr/static/react/precache-manifest.cab96ec522915e80f1bfca67c3928401.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5e0a432b40e0d8b1b4534f9ab5c640",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "2b905ce43307ab68d2c4",
+    "url": "/static/react/css/main.4d983085.chunk.css"
+  },
+  {
+    "revision": "612eb12aa68ef6d764fc",
+    "url": "/static/react/js/2.4735839e.chunk.js"
+  },
+  {
+    "revision": "34df8bf57a7ccea708b7e763165cc3ff",
+    "url": "/static/react/js/2.4735839e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b905ce43307ab68d2c4",
+    "url": "/static/react/js/main.23af6a42.chunk.js"
+  },
+  {
+    "revision": "c7241993fd2c7bed7213",
+    "url": "/static/react/js/runtime-main.b9f37f5d.js"
+  }
+]);
