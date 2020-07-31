bc5f71ac0d0ac8747c76e3bdeb2f124b32663d6a
Kwazinator <46730201+Kwazinator@users.noreply.github.com>
2020-07-31 16:31:37 -0400

    Update Home.js

--- react-frontend/src/Pages/Home.js
+++ react-frontend/src/Pages/Home.js
@@ -222,7 +222,7 @@ export default function ComplexGrid(props) {
                         >
                             HotKeys
                         </Typography>
-                        <img width={'20%'} height={'auto'} src={"/static/images/Shiftbutton.jpg"}/> <Typography display={'block'}>Undo</Typography>
+                        <img width={'20%'} height={'auto'} src={"/static/images/Control-512.png"}/> <Typography display={'block'}>Undo</Typography>
                         <img width={'20%'} height={'auto'} src={"/static/images/enterkey.png"}/> <Typography>Reset</Typography>
                     <Divider />
                     <div style={{marginTop:'100px'}}>
