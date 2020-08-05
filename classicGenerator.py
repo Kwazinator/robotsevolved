eacce186a9a0fe11acfa1603af82be4d0ad7795f
Kwazinator <kwasiky@gmail.com>
2020-08-05 10:27:18 -0400

    removing old static files

--- classicGenerator.py
+++ classicGenerator.py
@@ -27,25 +27,25 @@ if __name__ == "__main__":
             if (moves >= 23 and not godflag):
                 app = flaskr.create_app()
                 with app.app_context():
-                    GeneratorService().insert_puzzle('classic', 'GodTeir', generator.formatsolutiondata(solution), 'abcdefg', moves, json.dumps(solutiondata))
+                    GeneratorService().insert_puzzle('classic V2', 'GodTeir', generator.formatsolutiondata(solution), 'abcdefg', moves, json.dumps(solutiondata))
                     godflag = True
                     print('found turkutier puzzle of ' + str(moves) + ' moves')
             elif (17 < moves < 23 and not exteremlyhardflag):
                 app = flaskr.create_app()
                 with app.app_context():
-                    GeneratorService().insert_puzzle('classic', 'Exteremly Hard', generator.formatsolutiondata(solution), 'abcdefg', moves, json.dumps(solutiondata))
+                    GeneratorService().insert_puzzle('classic V2', 'Exteremly Hard', generator.formatsolutiondata(solution), 'abcdefg', moves, json.dumps(solutiondata))
                     exteremlyhardflag = True
                     print('found exteremlyhard puzzle of ' + str(moves) + ' moves')
             elif (13 < moves <= 17 and not hardflag):
                 app = flaskr.create_app()
                 with app.app_context():
-                    GeneratorService().insert_puzzle('classic', 'hard', generator.formatsolutiondata(solution), 'abcdefg', moves, json.dumps(solutiondata))
+                    GeneratorService().insert_puzzle('classic V2', 'hard', generator.formatsolutiondata(solution), 'abcdefg', moves, json.dumps(solutiondata))
                     hardflag = True
                     print('found hard puzzle of ' + str(moves) + ' moves')
             elif (8 < moves <= 13 and not mediumflag):
                 app = flaskr.create_app()
                 with app.app_context():
-                    GeneratorService().insert_puzzle('classic', 'medium', generator.formatsolutiondata(solution), 'abcdefg', moves, json.dumps(solutiondata))
+                    GeneratorService().insert_puzzle('classic V2', 'medium', generator.formatsolutiondata(solution), 'abcdefg', moves, json.dumps(solutiondata))
                     mediumflag = True
                     print('found medium puzzle of ' + str(moves) + ' moves')
             elif (5 <= moves <= 8 and not easyflag):
@@ -53,4 +53,4 @@ if __name__ == "__main__":
                 with app.app_context():
                     easyflag = True
                     print('found easy puzzle of ' + str(moves) + ' moves')
-                    GeneratorService().insert_puzzle('classic', 'easy', generator.formatsolutiondata(solution), 'abcdefg', moves, json.dumps(solutiondata))
+                    GeneratorService().insert_puzzle('classic V2', 'easy', generator.formatsolutiondata(solution), 'abcdefg', moves, json.dumps(solutiondata))
