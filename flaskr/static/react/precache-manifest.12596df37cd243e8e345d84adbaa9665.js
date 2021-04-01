5dff846f0ad5d1d81e4225bb6c8e28ff4ba8cdf7
Kwazinator <46730201+Kwazinator@users.noreply.github.com>
2021-04-01 12:31:50 -0400

    profile page changes

---
+++ flaskr/static/react/precache-manifest.12596df37cd243e8e345d84adbaa9665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0500c192fc8753a35defaea1c92cd272",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "8e0e35d857850d6a1d34",
+    "url": "/static/react/css/main.94810166.chunk.css"
+  },
+  {
+    "revision": "29484ff0fd17dfa079d4",
+    "url": "/static/react/js/2.8cf43494.chunk.js"
+  },
+  {
+    "revision": "cc184dce716f76b1adf2fe9dd1671a3c",
+    "url": "/static/react/js/2.8cf43494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e0e35d857850d6a1d34",
+    "url": "/static/react/js/main.c019e106.chunk.js"
+  },
+  {
+    "revision": "642bffd405cc52de04d1",
+    "url": "/static/react/js/runtime-main.8fa3ef7f.js"
+  }
+]);
