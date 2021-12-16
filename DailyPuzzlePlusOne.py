a308b2c1852ee4ab2b2111a0a7643e9ccbee3a65
Kwazinator <kwasiky@gmail.com>
2021-12-16 12:51:03 -0500

    Update DailyPuzzlePlusOne.py

--- DailyPuzzlePlusOne.py
+++ DailyPuzzlePlusOne.py
@@ -71,7 +71,7 @@ if __name__ == "__main__":
         # select game_id here
         #
         #
-        game = GameService().get_game(171)
+        game = GameService().get_game(57)
         parsedData = json.loads(game.puzzledata)
         wallsH = parsedData['wallHorizontal']
         wallsV = parsedData['wallVerticle']
