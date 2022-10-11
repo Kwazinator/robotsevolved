f70508a79b237f50cc142e4b2176eb1bf4e28d06
Kwazinator <kwasiky@gmail.com>
2022-10-10 23:53:32 -0400

    Update generatorEvolution.py

--- generatorEvolution.py
+++ generatorEvolution.py
@@ -27,7 +27,7 @@ def generateEvolutionPatterns():
 
     for i, nothing in enumerate(range(5)):
         for j, nothing2 in enumerate(range(5)):
-            if (random.randint(0, 12) > 5):
+            if (random.randint(0, 12) > 3):
                 randomnum = random.randint(0, 15)
                 countwalls += 1
                 goalspot = int(randomnum / 4)
